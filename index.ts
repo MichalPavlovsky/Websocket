a4f49958dd6256aa96ad296e603558b9c44a8770
Michal Pavlovsky <pavlovsky.miso@gmail.com>
2023-09-08 09:13:05 +0200

    third commit

--- index.ts
+++ index.ts
@@ -5,10 +5,24 @@ import configure from './routers';
 const app = express();
 const port = process.env.PORT || 3000;
 
+function onSocketPreError(e: Error) {
+    console.log(e);
+}
+
+function onSocketPostError(e: Error) {
+    console.log(e);
+}
+
 configure(app);
 
 console.log(`Attempting to run server on port ${port}`);
 
-app.listen(port, () => {
-    console.log(`Listenng on port ${port}`);
+const s = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
+
+const wss = new WebSocketServer({ noServer:true});
+
+s.on('upgrade', (req,socket,head) => {
+    socket.on('error', onSocketPreError)
+})
